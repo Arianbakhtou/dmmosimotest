8e60f30d482c0e4d815db9314fef95db831c3dfd
Nasir Hossain Akash <79259896+NasirHossainAkash@users.noreply.github.com>
2022-01-23 01:29:02 +0600

    fixed os environment

--- main.py
+++ main.py
@@ -6,10 +6,10 @@
 import os
 from pyrogram import Client
 
-API_ID=os.getenv["API_ID"]
-API_HASH=os.getenv["API_HASH"]
-BOT_TOKEN=os.getenv["BOT_TOKEN"]
-Username= os.getenv["USERNAME"]
+API_ID=os.environ["API_ID"]
+API_HASH=os.environ["API_HASH"]
+BOT_TOKEN=os.environ["BOT_TOKEN"]
+Username= os.environ["USERNAME"]
 
 app = Client("Telegram DM Forward Bot",api_id=int(API_ID),api_hash= API_HASH,bot_token=BOT_TOKEN)
 
